534f9bd2d545829c8bfcabf2277882aa67be568b
drew gordon <drew.s.gordon@gmail.com>
2014-08-21 00:44:41 -0400

    try it with txt only

--- parse_txt.py
+++ parse_txt.py
@@ -1,3 +1,25 @@
 def open_file(f):
-  with open(f, 'rb') as fopen:
-    fopen.readlines()
+	with open(f, 'rb') as fopen:
+		doc = fopen.read()
+
+		txtstr = ''
+
+		for line in doc:
+			toadd = ''
+
+			if line == '\n\n':
+				toadd = '|'
+				txtstr += toadd
+
+			else:
+				toadd = line
+				txtstr += toadd
+
+
+		txtlst = txtstr.split('||')
+
+		for e in txtlst:
+
+			print e, '===-=-=-====-=-=-=-=-=-=-='
+
+open_file('sandbox_docs/madagascar_test.txt')
