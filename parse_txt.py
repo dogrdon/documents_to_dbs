ba3bc696ad63dc97c14e342dfdb83ccf69d20213
drew gordon <drew.s.gordon@gmail.com>
2014-08-21 23:26:57 -0400

    trying some things out

--- parse_txt.py
+++ parse_txt.py
@@ -1,13 +1,40 @@
+from repr import repr
+import re
+
+
 def open_file(f):
 	with open(f, 'rb') as fopen:
-		doc = fopen.read()
+	    doc = fopen.read()
 
-		txtstr = ''
+	    txtstr = ''
 
+	    print type(doc)
+
+	    doc = doc.replace('\n', '|')
+	    doc = doc.replace('| |', '||')
+	    #doc = doc.replace('||', ',')
+	    #print doc
+
+	    sdoc = doc.split('|||')
+	    #but first change any number of tabs to a space?
+	    sdoc = [re.sub(r'\t+', ' ', i) for i in sdoc]
+	    sdoc = [i.replace('||', '\t') for i in sdoc]
+
+	    print len(sdoc)
+
+	    for x in sdoc:
+	    	print x, '==============\n'
+
+
+	    '''
 		for line in doc:
 			toadd = ''
 
-			if line == '\n\n':
+			if line == '\n\n\n':
+				toadd = '||'
+				txtstr += toadd
+
+			elif line == '\n\n':
 				toadd = '|'
 				txtstr += toadd
 
@@ -15,11 +42,14 @@ def open_file(f):
 				toadd = line
 				txtstr += toadd
 
-
+		print repr(txtstr)
+				
 		txtlst = txtstr.split('||')
 
-		for e in txtlst:
+		print len(txtlst)
+		'''
+		#for e in txtlst:
 
-			print e, '===-=-=-====-=-=-=-=-=-=-='
-
+		#	print e, '===-=-=-====-=-=-=-=-=-=-='
+		
 open_file('sandbox_docs/madagascar_test.txt')
